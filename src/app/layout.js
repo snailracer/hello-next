6ed5567541605aed1109235a430a06086ea42d4e
QUEEN\jimr <jimr@pageuppeople.com>
2023-12-22 11:41:17 +1030

    -add time

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,11 +3,12 @@ import React from 'react';
 import './styles.css';
 
 function RootLayout({ children }) {
+    const timeStamp = new Date().toLocaleString()
   return (
     <html lang="en">
       <body>
         {children}
-
+        <footer>This page was rendered @ {timeStamp}</footer>
       </body>
     </html>
   );
